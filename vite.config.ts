168733e60fdc76d864f1a7c6ffe5168a4fe781a5
saligia28 <1531583769@qq.com>
2025-11-26 15:05:09 +0800

    feat:配置细节调整

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
     port: 5173,
+    strictPort: false, // 如果端口被占用，自动尝试下一个可用端口
+    host: true, // 监听所有地址
   },
   preview: {
     host: '0.0.0.0',
